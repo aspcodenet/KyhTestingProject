be145e2fb7b4a11de616f8d29e9f1430c7fc70f5
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-05-31 12:52:13 +0200

    tester

--- KyhTestingProject.Tests/Services/SwedishBeerServiceTests.cs
+++ KyhTestingProject.Tests/Services/SwedishBeerServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using KyhTestingProject.Services;
+﻿using System;
+using KyhTestingProject.Services;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace KyhTestingProject.Tests.Services;
@@ -26,5 +27,74 @@ public class SwedishBeerServiceTests
         // ASSERT
         Assert.IsFalse(result);
     }
+
+    [TestMethod]
+    public void If_promille_less_than_one_and_age_18_and_krogen_return_true()
+    {
+        //ARRANGE
+        var promille = 0.1m;
+        var location = Location.Krogen;
+        var age = 18;
+
+        // ACT
+        var result = _sut.CanIBuyBeer(age, location, promille);
+
+        // ASSERT
+        Assert.IsTrue(result);
+
+    }
+
+    [TestMethod]
+    public void If_promille_less_than_one_and_age_17_and_krogen_return_false()
+    {
+        //ARRANGE
+        var promille = 0.1m;
+        var location = Location.Krogen;
+        var age = 17;
+
+        // ACT
+        var result = _sut.CanIBuyBeer(age, location, promille);
+
+        // ASSERT
+        Assert.IsFalse(result);
+    }
+
+
+
+
+    [TestMethod]
+    public void If_promille_less_than_one_and_age_20_and_systemet_return_true()
+    {
+        //ARRANGE
+        var promille = 0.1m;
+        var location = Location.Systemet;
+        var age = 20;
+
+        // ACT
+        var result = _sut.CanIBuyBeer(age, location, promille);
+
+        // ASSERT
+        Assert.IsTrue(result);
+    }
+
+
+
+    [TestMethod]
+    public void If_promille_less_than_one_and_age_19_and_systemet_return_false()
+    {
+        //ARRANGE
+        var promille = 0.1m;
+        var location = Location.Systemet;
+        var age = 19;
+
+        // ACT
+        var result = _sut.CanIBuyBeer(age, location, promille);
+
+        // ASSERT
+        Assert.IsFalse(result);
+    }
+
+
+
 
 }
