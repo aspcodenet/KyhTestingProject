100ba3a5783140209606fbe79e41af2c719a5787
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-05-31 10:59:02 +0200

    tests

---
+++ KyhTestingProject.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KyhTestingProject.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
