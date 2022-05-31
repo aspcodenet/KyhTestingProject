4f1676380584031504bd260dad7d169d8b6616ed
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-05-31 13:36:49 +0200

    books

---
+++ KyhTestingProject.Tests/Library/BookTests.cs
@@ -0,0 +1,102 @@
+﻿using KyhTestingProject.Library;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KyhTestingProject.Tests.Library;
+
+[TestClass]
+public class BookTests
+{
+    [TestMethod]
+    public void When_borrowing_available_count_should_decrease_by_one()
+    {
+        //ARRANGE
+        var sut = new Book("123231231", "JK...", "Harry Potter");
+        sut.BuyNewEx(10);
+
+        //ACT
+        sut.Borrow("stefan");
+
+        //ASSERT
+        Assert.AreEqual(9, sut.Available);
+
+    }
+
+    [TestMethod]
+    public void When_borrowing_twice_available_count_should_decrease_by_two()
+    {
+        //ARRANGE
+        var sut = new Book("123231231", "JK...", "Harry Potter");
+        sut.BuyNewEx(10);
+
+        //ACT
+        sut.Borrow("stefan");
+        sut.Borrow("lisa");
+
+        //ASSERT
+        Assert.AreEqual(8, sut.Available);
+
+    }
+
+    [TestMethod]
+    public void When_borrowing_the_borrower_is_added_to_the_list()
+    {
+        var sut = new Book("123231231", "JK...", "Harry Potter");
+        sut.BuyNewEx(10);
+
+        //ACT
+        sut.Borrow("stefan");
+
+
+        Assert.IsTrue(sut.IsBorrower("stefan"));
+
+
+
+    }
+
+
+    [TestMethod]
+    public void Contructor_should_set_correct_values()
+    {
+        //ARRANGE + ACT
+        var sut = new Book("231231", "Stefan", "Hockey idols");
+
+        // ASSERT
+        Assert.AreEqual("231231", sut.Isbn);
+        Assert.AreEqual("Stefan", sut.Author);
+        Assert.AreEqual("Hockey idols", sut.Title);
+        Assert.AreEqual(0, sut.Count);
+    }
+
+    [TestMethod]
+    public void Cant_borrow_book_when_no_available()
+    {
+        var sut = new Book("231231", "Stefan", "Hockey idols");
+
+        var result = sut.Borrow("Stefan");
+
+        Assert.IsFalse(result);
+    }
+
+
+    //[TestMethod]
+    //public void Cant_borrow_book_when_already_borrowed()
+    //{
+    //    var sut = new Book("231231", "Stefan", "Hockey idols");
+    //    sut.BuyNewEx(10);
+
+    //    sut.Borrow("Stefan");
+    //    var result = sut.Borrow("Stefan");
+
+    //    Assert.IsFalse(result);
+    //}
+
+
+
+
+
+
+    //Låna en bok om  alla är utlånade ska inte gå
+
+
+
+}
---
+++ KyhTestingProject/Library/Book.cs
@@ -0,0 +1,81 @@
+﻿namespace KyhTestingProject.Library;
+
+public class Book
+{
+    public string Isbn { get; }
+    public string Author { get; }
+
+    public string Title{ get; }
+
+    public int Count { get; protected set; }
+    public int Borrowed => borrowers.Count;
+
+    public bool Borrow(string borrower)
+    {
+        if (borrowers.Count() >= Count)   return false;
+        borrowers.Add(borrower);
+        return true;
+    }
+
+    public bool IsBorrower(string borrower)
+    {
+        return borrowers.Contains(borrower);
+    }
+
+    public void BuyNewEx(int count)
+    {
+        Count += count;
+    }
+
+    private List<string> borrowers = new List<string>();
+
+    public int Available => Count - Borrowed;
+
+    public Book(string isbn, string author, string title)
+    {
+        Isbn = isbn;
+        Author = author;
+        Count = 0;
+        Title = title;
+    }
+}
+
+/*För att förklara hur det används så är den ungefär såhär
+		
+
+    sut = new Book();
+    sut.Title = "Harry";
+    Assert.AreEqual("Harry", sut.Title);
+
+    ```
+{
+    var books = new List<Book>
+    {
+        new Book("12223","Nils Nilsson", "Fantastiska dikter"),
+        new Book("55551234","Åsa Åsasson", "Mina bästa TV-spel")
+    };
+
+    books[0].BuyNewEx(12);
+    books[1].BuyNewEx(2);
+
+    var borrower = "Stefan";
+    if(books[0].IsBorrower(borrower) == false)
+        books[0].Borrow(borrower);
+    else
+    {
+        Console.WriteLine("Du har ju redan lånat den boken");
+    }
+    Console.WriteLine($"Antal kvar av {books[0].Title} är nu {books[0].Count} ");
+                
+        ```
+
+		
+
+
+
+
+    -att det inte går att låna en bok ifall alla är utlånade
+        - att man blir  en borrower när man lånar en bok
+        - att antalet ex ökar när man köper in nya exemplar
+
+*/
