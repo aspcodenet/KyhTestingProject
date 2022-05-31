feaa3ba7308f61f54c3940b41d6948227570a355
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-05-31 10:51:41 +0200

    beer2

---
+++ KyhTestingProject/Services/IBeerService.cs
@@ -0,0 +1,32 @@
+﻿namespace KyhTestingProject.Services;
+public interface IBeerService
+{
+    bool CanIBuyBeer(int age, Location location, decimal promilleHalt);
+}
+public enum Location
+{
+    Krogen,
+    Systemet
+}
+public class SwedishBeerService : IBeerService
+{
+    public bool CanIBuyBeer(int age, Location location, decimal promilleHalt)
+    {
+        if (promilleHalt > 1.0m) return false;
+        if (age >= 18 && location == Location.Krogen) return true;
+        if (age >= 20 && location == Location.Systemet) return true;
+        return false;
+    }
+}
+public class NorwegianBeerService : IBeerService
+{
+    public bool CanIBuyBeer(int age, Location location, decimal promilleHalt)
+    {
+        if (promilleHalt > 1.5m) return false;
+        if (age >= 18 && location == Location.Krogen) return true;
+        if (age >= 20 && location == Location.Systemet) return true;
+        return false;
+    }
+}
+
+
