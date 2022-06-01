ff90aed20363d9c78a358275d326258860d0c713
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-06-01 12:54:04 +0200

    Med Fake and Stubs

---
+++ KyhTestingProject.Tests/Services/RegistrationServiceTests.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using KyhTestingProject.Services;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KyhTestingProject.Tests.Services;
+
+public class FakeUserRepository : IUserRepository
+{
+    public List<string> Existing { get; set; } = new List<string>();
+
+    public bool Exists(string email)
+    {
+        return Existing.Contains(email);
+    }
+
+    public int GetRegistreredCountToday()
+    {
+        return Existing.Count;
+    }
+}
+
+public class FakeEmailService : IEmailService
+{
+    public bool HasBeenCalled = false;
+    public void SendWelcomeEmail(string email)
+    {
+        HasBeenCalled = true;
+    }
+}
+
+[TestClass]
+public class RegistrationServiceTests
+{
+    private RegistrationService _sut;
+    private FakeUserRepository _repository;
+    private FakeEmailService _emailService;
+    public RegistrationServiceTests()
+    {
+        _repository = new FakeUserRepository();
+        _emailService = new FakeEmailService();
+
+        _sut = new RegistrationService(_repository,  _emailService );
+    }
+
+    [TestMethod]
+    public void Should_not_allow_registration_if_already_registered()
+    {
+        //ARRANGE
+        var email = "stefan@hej.se";
+        _repository.Existing.Clear();
+        _repository.Existing.Add(email);
+        
+        //ACT
+        var result = _sut.Register(email);
+
+        //ASSERT
+        Assert.AreEqual(RegistrationStatus.AlreadyRegistered, result);
+    }
+
+
+
+
+
+
+    [TestMethod]
+    public void Should_not_allow_registration_if_more_than_10_today()
+    {
+        //ARRANGE
+        _repository.Existing.Clear();
+        for(int i = 0; i < 11; i++)
+            _repository.Existing.Add(i.ToString() + "@asdas.se" );
+
+        //ACT
+        var result = _sut.Register("sss@hej.se");
+
+        //ASSERT
+        Assert.AreEqual(RegistrationStatus.TooManyRegistrationsToday, result);
+    }
+
+
+
+    [TestMethod]
+    public void When_email_is_not_correct_domain_should_fail()
+    {
+        //ARRANGE
+
+        //ACT
+        var result = _sut.Register("blabla@aaa.se");
+
+        //ASSERT
+        Assert.AreEqual(RegistrationStatus.WrongDomain, result);
+    }
+
+
+    [TestMethod]   
+    public void When_registration_is_ok_we_get_ok_result()
+    {
+        //ARRANGE
+        _repository.Existing.Clear();
+
+        //ACT
+        var result = _sut.Register("blabla@hej.se");
+
+        //ASSERT
+        Assert.AreEqual(RegistrationStatus.Ok, result);
+    }
+
+
+
+    [TestMethod]   // when ok så skall vi delegera till emailservicen
+    public void When_registration_is_ok_an_email_should_be_sent()
+    {
+        //ARRANGE
+        _repository.Existing.Clear();
+
+        //ACT
+        var result = _sut.Register("blabla@hej.se");
+
+        //ASSERT
+        Assert.IsTrue( _emailService.HasBeenCalled );
+    }
+
+
+
+}
---
+++ KyhTestingProject/Services/IRegistrationService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace KyhTestingProject.Services;
+
+public interface IRegistrationService
+{
+    RegistrationStatus Register(string email);
+}
+
+public enum RegistrationStatus
+{
+    Ok,
+    AlreadyRegistered,
+    WrongDomain,
+    TooManyRegistrationsToday
+}
+
+public interface IUserRepository
+{
+    bool Exists(string email);
+
+    int GetRegistreredCountToday();
+}
+
+
+public interface IEmailService
+{
+    void SendWelcomeEmail(string email);
+}
+
+
+public class RegistrationService : IRegistrationService
+{
+    private readonly IUserRepository _userRepository;
+    private readonly IEmailService _emailService;
+
+    public RegistrationService(IUserRepository userRepository, IEmailService emailService)
+    {
+        _userRepository = userRepository;
+        _emailService = emailService;
+    }
+    public RegistrationStatus Register(string email)
+    {
+
+        if (AlreadyRegistered(email))
+            return RegistrationStatus.AlreadyRegistered;
+
+        if (!CorrectDomain(email))
+            return RegistrationStatus.WrongDomain;
+
+        if (TooManyToday())
+            return RegistrationStatus.TooManyRegistrationsToday;
+
+        _emailService.SendWelcomeEmail(email);
+        return RegistrationStatus.Ok;
+    }
+
+    private bool TooManyToday()
+    {
+        return _userRepository.GetRegistreredCountToday() > 10;
+    }
+
+    private bool CorrectDomain(string email)
+    {
+        return email.EndsWith("hej.se")|| email.EndsWith("hej.com");
+    }
+
+    private bool AlreadyRegistered(string email)
+    {
+        return _userRepository.Exists(email);
+    }
+}
