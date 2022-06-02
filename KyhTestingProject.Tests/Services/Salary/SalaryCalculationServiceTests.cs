eb0351e5946b458457ddbe6b845cb8060681c320
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-06-02 10:48:14 +0200

    a

--- KyhTestingProject.Tests/Services/Salary/SalaryCalculationServiceTests.cs
+++ KyhTestingProject.Tests/Services/Salary/SalaryCalculationServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using KyhTestingProject.Data;
 using KyhTestingProject.Services.Salary;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -10,21 +11,34 @@ public class FakeEmployeeRepository : IEmployeeRepository
     public List<string> SSNs { get; set; } = new List<string>();
     public Employee? GetBySSN(string ssn)
     {
-        if (SSNs.Contains(ssn)) return new Employee();
+        if (SSNs.Contains(ssn)) return new Employee {Started = DateTime.Now.AddYears(-3)};
         return null;
     }
 }
-
+public class FakePaymentService : IPaymentService
+{
+    public bool HasBeenCalled = false;
+    public string SSN;
+    public decimal Salary;
+    public void Pay(string ssn, decimal salary)
+    {
+        SSN = ssn;
+        Salary = salary;
+        HasBeenCalled = true;
+    }
+}
 [TestClass]
 public class SalaryCalculationServiceTests
 {
     private SalaryCalculationService _sut;
     private FakeEmployeeRepository _employeeRepository;
+    private FakePaymentService _fakePaymentService;
 
     public SalaryCalculationServiceTests()
     {
+        _fakePaymentService = new FakePaymentService();
         _employeeRepository = new FakeEmployeeRepository();
-        _sut = new SalaryCalculationService(_employeeRepository);
+        _sut = new SalaryCalculationService(_employeeRepository, _fakePaymentService);
     }
 
     [TestMethod]
@@ -34,7 +48,7 @@ public class SalaryCalculationServiceTests
         _employeeRepository.SSNs.Clear();
 
         //ACT
-        var result = _sut.CalculateService("3321231231", 12);
+        var result = _sut.CalculateService("19720101-0101", 12);
         //ASSERT
         Assert.AreEqual(SalaryCalculationStatus.EmployeeNotFound,result);
     }
@@ -44,14 +58,43 @@ public class SalaryCalculationServiceTests
     public void When_ssn_is_found_in_database_should_return_ok()
     {
         // ARRANGE
-        _employeeRepository.SSNs.Add("3");
+        _employeeRepository.SSNs.Add("19720101-0101");
         //ACT
-        var result = _sut.CalculateService("3", 12);
+        var result = _sut.CalculateService("19720101-0101", 12);
         //ASSERT
         Assert.AreEqual(SalaryCalculationStatus.Ok, result);
     }
 
 
+    [TestMethod]
+    public void When_ok_a_salary_payment_is_made()
+    {
+        // ARRANGE
+        _employeeRepository.SSNs.Add("19720101-0101");
+        _fakePaymentService.HasBeenCalled = false;
+
+        //ACT
+        var result = _sut.CalculateService("19720101-0101", 12);
+        //ASSERT
+        //Verifiera att en betalning är gjord
+        Assert.IsTrue(_fakePaymentService.HasBeenCalled);
+    }
+
+
+    [TestMethod]
+    public void Salary_Is_Calculated_Correctly()
+    {
+        //ARRANGE
+        _employeeRepository.SSNs.Add("19720101-0101");
+        _fakePaymentService.SSN = "";
+        _fakePaymentService.Salary = 0;
+
+        var result = _sut.CalculateService("19720101-0101", 12);
+
+        //VERIFIERA ATT Salary som beräknades där inuti är 1560
+        Assert.AreEqual(1560, _fakePaymentService.Salary);
+        Assert.AreEqual("19720101-0101", _fakePaymentService.SSN);
+    }
 
 
 
--- KyhTestingProject/Services/Salary/ISalaryCalculationService.cs
+++ KyhTestingProject/Services/Salary/ISalaryCalculationService.cs
@@ -19,21 +19,33 @@ public interface IEmployeeRepository
     Employee? GetBySSN(string ssn);
 }
 
+public interface IPaymentService
+{
+    void Pay(string ssn, decimal salary);
+}
+
 public class SalaryCalculationService : ISalaryCalculationService
 {
     private readonly IEmployeeRepository _employeeRepository;
+    private readonly IPaymentService _paymentService;
 
-    public SalaryCalculationService(IEmployeeRepository employeeRepository)
+    public SalaryCalculationService(IEmployeeRepository employeeRepository,
+        IPaymentService paymentService)
     {
         _employeeRepository = employeeRepository;
+        _paymentService = paymentService;
     }
     public SalaryCalculationStatus CalculateService(string ssn, int antalTimmar)
     {
         var employee = _employeeRepository.GetBySSN(ssn);
         if (employee == null)
             return SalaryCalculationStatus.EmployeeNotFound;
-        // salary = antaltimmar *´....
-        // paymentService.Pay(ssn, salary)
+
+
+        var antalYears = DateTime.Now.Year - employee.Started.Year;
+        var faktor = 100.0m * (1.0m + (antalYears / 10.0m));
+        var salary = antalTimmar * faktor;
+        _paymentService.Pay(ssn, salary);
         return SalaryCalculationStatus.Ok;
     }
 }
