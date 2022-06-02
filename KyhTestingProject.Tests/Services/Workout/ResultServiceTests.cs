db991b63892d23f50ef4c47cf2b2e7b48cc9917e
Stefan Holmberg <stefan.holmberg@systementor.se>
2022-06-02 14:34:46 +0200

    Result

---
+++ KyhTestingProject.Tests/Services/Workout/ResultServiceTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using KyhTestingProject.Services.Workout;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace KyhTestingProject.Tests.Services.Workout;
+
+public class FakeUserService : IUserService
+{
+    public List<Guid> Existing { get; set; } = new List<Guid>();
+
+    public bool Exists(Guid workoutUserId)
+    {
+        return Existing.Contains(workoutUserId);
+    }
+}
+
+[TestClass]
+public class ResultServiceTests
+{
+    private ResultService _sut;
+    private FakeUserService _fakeUserService;
+    public ResultServiceTests()
+    {
+        _fakeUserService = new FakeUserService();
+        _sut = new ResultService(_fakeUserService);
+    }
+
+    [TestMethod]
+    public void When_user_is_not_found_error_will_be_returned()
+    {
+        //ARRANGE
+        var guid = Guid.NewGuid();
+        _fakeUserService.Existing.Clear();
+
+        var workoutModel = new WorkoutResult
+        {
+            UserId = guid,
+            TotalMeters = 1000,
+            Start = DateTime.Now.AddHours(-5),
+            Slut = DateTime.Now.AddHours(-4),
+        };
+        
+        //ACT
+        var result = _sut.Register(workoutModel);
+        
+        //ASSERT
+        Assert.AreEqual(WorkoutRegistrationStatus.NoSuchUser,result);
+    }
+
+
+
+    [TestMethod]
+    public void When_user_is_found_ok_will_be_returned()
+    {
+        //ARRANGE
+        var guid = Guid.NewGuid();
+        _fakeUserService.Existing.Clear();
+        _fakeUserService.Existing.Add(guid);
+
+        var workoutModel = new WorkoutResult
+        {
+            UserId = guid,
+            TotalMeters = 1000,
+            Start = DateTime.Now.AddHours(-5),
+            Slut = DateTime.Now.AddHours(-4),
+        };
+
+        //ACT
+        var result = _sut.Register(workoutModel);
+
+        //ASSERT
+        Assert.AreEqual(WorkoutRegistrationStatus.Ok, result);
+    }
+
+
+}
---
+++ KyhTestingProject/Services/Workout/IResultService.cs
@@ -0,0 +1,79 @@
+﻿namespace KyhTestingProject.Services.Workout;
+
+public interface IResultService
+{
+    WorkoutRegistrationStatus Register(WorkoutResult workout);
+}
+public class WorkoutResult
+{
+    public Guid UserId { get; set; }
+    public DateTime Start { get; set; }
+    public DateTime Slut { get; set; }
+    public int TotalMeters { get; set; }
+}
+public enum WorkoutRegistrationStatus
+{
+    Ok,
+    NoSuchUser,
+}
+
+public interface IUserService
+{
+    bool Exists(Guid workoutUserId);
+}
+
+public interface ICalculationService
+{
+    TimeSpan CalculateVelocityPerKm(DateTime workoutStart, 
+        DateTime workoutSlut, int workoutTotalMeters);
+}
+
+public interface IStatisticsService
+{
+    TimeSpan GetCurrentRecord(Guid workoutUserId);
+    void SaveNewRecord(Guid workoutUserId, TimeSpan speed);
+}
+
+public interface IEmailService
+{
+    void SendRecordMail(Guid workoutUserId);
+}
+
+public class ResultService : IResultService
+{
+    private readonly IUserService _userService;
+    private readonly ICalculationService _calculationService;
+    private readonly IStatisticsService _statisticsService;
+    private readonly IEmailService _emailService;
+
+    public ResultService(IUserService userService, 
+        ICalculationService calculationService,
+        IStatisticsService statisticsService,
+        IEmailService emailService )
+    {
+        _userService = userService;
+        _calculationService = calculationService;
+        _statisticsService = statisticsService;
+        _emailService = emailService;
+    }
+    public WorkoutRegistrationStatus Register(WorkoutResult workout)
+    {
+        if (!_userService.Exists(workout.UserId))
+            return WorkoutRegistrationStatus.NoSuchUser;
+
+
+        var speedPerKm = _calculationService.CalculateVelocityPerKm(workout.Start, workout.Slut,
+            workout.TotalMeters);
+
+        var currentFastestPerKm = _statisticsService.GetCurrentRecord(workout.UserId);
+        if (speedPerKm < currentFastestPerKm)
+        {
+            _statisticsService.SaveNewRecord(workout.UserId, speedPerKm);
+            _emailService.SendRecordMail(workout.UserId);
+        }
+
+        return WorkoutRegistrationStatus.Ok;
+    }
+    //Fler
+
+}
